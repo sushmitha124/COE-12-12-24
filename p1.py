25c930a6fb3bf8c499fc6d57c2ffee4548dde265
sushmitha124 <pochamsushmitha5@gmail.com>
2024-12-12 19:34:34 +0530

    third commit

--- p1.py
+++ p1.py
@@ -4,5 +4,5 @@ bill2=int(input("enter bill2"))
 bill3=int(input("enter bill3"))
 total_bill=bill1+bill2+bill3
 print("The total bill is",total_bill)
-percentage=(total_bill*100)/sal*100
+percentage=(total_bill*100)/sal
 print("The total percentage is",percentage)
